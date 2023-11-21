efef137d1714c39915986d95a56cfd5e9f98f1d5
Nidhimeena123 <nidhimeenasm123@gmail.com>
2023-11-21 12:51:51 +0530

    merge other

---
+++ Assignment/Module2.2/swap_without_thirdVariable.c
@@ -0,0 +1,12 @@
+//swapping of two integers without using third variable
+#include<stdio.h>
+int main(){
+	
+	int a,b;
+	printf("enter the value of a and b \n");
+	scanf("%d %d",&a,&b);
+    a= 
+	printf("before swapping  a=%d and b=%d",a,b);
+	printf("\nafter swapping  a=%d and b=%d",a,b);
+	return 0;
+}
